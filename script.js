5e2c18309c35c8a39975c7606d7b0e8b7459f46f
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-06-11 12:26:17 -0300

    Finalizando

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-const display = document.getElementById('display');
+const display = document.querySelector('#display');
+const keys = document.querySelectorAll('[id*=key]');
+const operators = document.querySelectorAll('[id*=Operator]');
 
 let newNumber = true;
 let operator;
 let previousNumber;
 
-function updateDisplay(numero) {}
+function updateDisplay(number) {
+    if (newNumber) {
+        display.textContent = number;
+        newNumber = false;
+    }
+    else display.textContent += number;
+}
 
-const insertNumber = (event) => {}
+const insertNumber = ({ target }) =>
+    updateDisplay(target.textContent);
 
-const selectOperator = (event) => {}
+keys.forEach(key => key.addEventListener('click', insertNumber));
 
-const calculate = () => {}
+const selectOperator = (event) => {
+    newNumber = true;
+    operator = event.target.textContent;
+    previousNumber = display.textContent;
+}
 
-const equal = document.querySelector("#igual");
+operators.forEach(operator => operator.addEventListener("click", selectOperator));
+
+const calculate = () => {
+    const actualNumber = display.textContent;
+    const result = eval(`${previousNumber}${operator}${actualNumber}`); //template string
+    newNumber = true;
+    updateDisplay(result);
+}
+
+const equal = document.querySelector("#equal");
 
 equal.addEventListener('click', calculate);
 
 const clearDisplay = () => display.textContent = "";
 
-document.querySelector("#limparDisplay").addEventListener("click", clearDisplay);
+document.querySelector("#clearDisplay").addEventListener("click", clearDisplay);
 
-const clearCalc = () => {};
+const clearCalc = () => {
+    clearDisplay();
+    newNumber = true;
+    operator = undefined;
+    previousNumber = undefined;
+};
 
-document.querySelector("#limparCalculo").addEventListener("click", clearCalc);
+document.querySelector("#clearCalc").addEventListener("click", clearCalc);
 
-const removeLastNumber = () => {};
+const removeLastNumber = () => {
+    newNumber = true;
+    updateDisplay(display.textContent.slice(0, -1));
+}
 
 document.querySelector("#backspace").addEventListener("click", removeLastNumber);
 
-const invertSignal = () => {}
+const invertSignal = () =>{
+    newNumber = true;
+    updateDisplay(display.textContent * -1);
+}
+
 
 document.querySelector("#inverter").addEventListener("click", invertSignal);
